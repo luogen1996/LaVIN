7bdbd1b2ea4dee203f244e72b97f73f1bad3cf03
luogen1996 <38316576+luogen1996@users.noreply.github.com>
2023-05-22 17:17:39 +0800

    update bash file

--- scripts/vl_instruction_tuning_vicuna_13b.sh
+++ scripts/vl_instruction_tuning_vicuna_13b.sh
@@ -1,5 +1,4 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.launch --nproc_per_node 8 train.py \
-    --model Llama7B_adapter \
     --llm_model 13B\
     --llama_model_path ../data/weights/ \
     --data_path ../data/alpaca_data.json \
